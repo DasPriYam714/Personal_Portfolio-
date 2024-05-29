b83cacd7c0cbdb673412c86993d50d264bc0bd9c
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-05-29 21:11:59 +0600

    add projects part 2

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,14 @@ import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
 import Profile from "@/components/Profile";
 import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav
-        navItems={[{name: 'Home', link:'/', icon: <FaHome></FaHome>}]}></FloatingNav>
+        navItems={navItems}></FloatingNav>
       <Hero></Hero>
       {/* <Profile></Profile> */}
 
