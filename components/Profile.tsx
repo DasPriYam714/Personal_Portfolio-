4886c126f7b644590bb2c4452f1ae53648e29fb5
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-06-12 00:52:48 +0600

    add solve

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,12 +1,12 @@
-'use client'
-import React from 'react';
+"use client"
+import React from "react";
 
-import Link from 'next/link';
-import { profileItems } from '@/data';
-import { BentoGridItem } from './ui/bento-grid';
-import { BackgroundGradient } from './ui/background-gradient';
-import { FaNodeJs,FaPhp, FaReact } from 'react-icons/fa';
-import {  SiNextdotjs, SiPostgresql, SiTailwindcss } from 'react-icons/si';
+import Link from "next/link";
+import { profileItems } from "@/data";
+import { BentoGridItem } from "./ui/bento-grid";
+import { BackgroundGradient } from "./ui/background-gradient";
+import { FaNodeJs,FaPhp, FaReact } from "react-icons/fa";
+import {  SiNextdotjs, SiPostgresql, SiTailwindcss } from "react-icons/si";
 import { ImGithub } from "react-icons/im";
 import { LuLinkedin } from "react-icons/lu";
 import { RiFacebookCircleLine } from "react-icons/ri";
@@ -24,25 +24,25 @@ interface Items{
 const Profile = () => {
     
     return (
-        <div id='profile' >
+        <div id="profile" >
        <BackgroundGradient className="rounded-[22px] max-w-150px p-2 sm:p-10 bg-white dark:bg-zinc-900 justify-center  ">
-       <div className='flex flex-col mb-4 md:flex-row flex-between justify-items-center justify-center md:justify-between  '>
+       <div className="flex flex-col mb-4 md:flex-row flex-between justify-items-center justify-center md:justify-between  ">
        
-       <div className='flex flex-col text-justify justify-center w-[60vh] text-[40px] mb-5 md:text-4xl lg:text-5xl  md:w-[70vh] '>
-       <div className='text-center justify-center'>
-       <h1>Hi! I'm <span className='text-blue-500 dark:text-blue-500'>Priyam</span></h1>
+       <div className="flex flex-col text-justify justify-center w-[60vh] text-[40px] mb-5 md:text-4xl lg:text-5xl  md:w-[70vh]">
+       <div className="text-center justify-center">
+       <h1>Hi! I am <span className="text-blue-500 dark:text-blue-500">Priyam</span></h1>
        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
           Lets Meet Together.
         </p>
        </div>
  
         <p className="text-sm text-neutral-600 dark:text-neutral-400 justify-center sm:text-wrap sm:mb-4 sm:ml-5 md:mr-5">
-        I'm a frontend-focused full-stack junior developer with expertise in JavaScript, React, Next.js, and Nest.js. Passionate about crafting dynamic web applications, I thrive on challenges, bringing innovative solutions through problem-solving skills. Proficient in manual software testing, I ensure high-quality releases and collaborate effectively with developers. As a driven computer science graduate, I'm committed to continuous learning and staying updated with industry trends. With a strong foundation in the MERN stack, I'm eager to make an impact in the IT industry, combining my development and testing skills to deliver robust solutions. My collaborative approach, problem-solving abilities, and attention to detail equip me to excel in any project. I value effective communication and teamwork and am excited to contribute my skills and creativity to drive technological innovation forward.
+        I am a frontend-focused full-stack junior developer with expertise in JavaScript, React, Next.js, and Nest.js. Passionate about crafting dynamic web applications, I thrive on challenges, bringing innovative solutions through problem-solving skills. Proficient in manual software testing, I ensure high-quality releases and collaborate effectively with developers. As a driven computer science graduate, I am committed to continuous learning and staying updated with industry trends. With a strong foundation in the MERN stack, I am eager to make an impact in the IT industry, combining my development and testing skills to deliver robust solutions. My collaborative approach, problem-solving abilities, and attention to detail equip me to excel in any project. I value effective communication and teamwork and am excited to contribute my skills and creativity to drive technological innovation forward.
         </p>
        </div>
-       <div className='justify-center lg:w-[55vh] sm:mt-5 md:justify-center item-center '>
+       <div className="justify-center lg:w-[55vh] sm:mt-5 md:justify-center item-center ">
        <img
-          src='priyam.png'
+          src="priyam.png"
           alt="Priyam"
           height="600"
           width="600"
@@ -86,7 +86,7 @@ const Profile = () => {
         </div>
         </div>
 
-        <div className='mt-10px'>
+        <div className="mt-10px">
           <h2 className="text-base uppercase font-titleFont mb-4 mt-5">
             Find me in
           </h2>
